1ca47b3670e485571179a316c4fea26e9faf2f33
manojpanta <manojpanta95@gmail.com>
2021-07-12 10:37:39 -0500

    Reversedigit math way

--- src/main/java/coding/challenges/ReverseDigit.java
+++ src/main/java/coding/challenges/ReverseDigit.java
@@ -4,11 +4,16 @@ public class ReverseDigit {
 
 
     private static int reverseDigit(int i) {
-
-        return 4321;
+        int toReturn = 0;
+        while(i > 0) {
+            int pop = i % 10;
+            toReturn = (toReturn * 10) + pop;
+            i = i /10;
+        }
+        return toReturn;
     }
 
     public static void main(String[] args) {
-        System.out.println(reverseDigit(1234));
+        System.out.println(reverseDigit(1234567890));
     }
 }
