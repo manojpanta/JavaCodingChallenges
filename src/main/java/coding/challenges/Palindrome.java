58cf607c96c4a9b0ce16f734dad910c98da4627f
manojpanta <manojpanta95@gmail.com>
2021-07-12 11:41:48 -0500

    Palindrome java exercise

---
+++ src/main/java/coding/challenges/Palindrome.java
@@ -0,0 +1,12 @@
+package coding.challenges;
+
+public class Palindrome {
+
+    private static boolean isPalindrome(String s) {
+        return  true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("racecar"));
+    }
+}
