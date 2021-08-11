4a55bcfe585fd261f798341ae9b9ba88cc91c4b0
manojpanta <manojpanta95@gmail.com>
2021-08-11 16:24:23 -0500

    Complete a2b2c2a2 challenge

---
+++ src/main/java/coding/challenges/A2B3C4.java
@@ -0,0 +1,26 @@
+package coding.challenges;
+
+public class A2B3C4 {
+
+    public static void main(String[] args) {
+//        given a String aabbccddaa return a string a2b2c2d2a2
+        System.out.println(solution("aaabbbcccdddaaa"));
+    }
+
+    private static String  solution(String s) {
+        char [] characters = s.toCharArray();
+        int i = 0;
+        String ss = "";
+        while(i < characters.length) {
+            int counter = 1;
+            while(i < characters.length - 1 && characters[i] == characters[i + 1]) {
+                counter ++;
+                i ++;
+            }
+            ss += Character.toString(characters[i]) + counter;
+            i = i + 1;
+        }
+        return  ss;
+
+    }
+}
